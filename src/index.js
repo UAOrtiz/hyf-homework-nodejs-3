27f8f0f7e581732c95cb953e34a6fa2c02ac5de5
UAOrtiz <uortiz@unitedvirtualities.com>
2019-12-04 10:10:20 -0700

    s

--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ app.get('/users', (req, res) => {
   res.json(users);
 });
 
+app.post('/users', function(req, res) {
+  users = [{
+    id: 0
+  }];
+  res.json({
+    StatusCode: 200,
+    body: users[users.length - 1],
+  });
+});
+
+app.get('/users', function(req, res) {
+  if (users.length === 0) {
+    res.json([{
+      id: 0
+    }]);
+  } else {
+    res.json([users[users.length - 1]]);
+  }
+});
+
 app.listen(3000, () => {
   console.log('Server is running');
 });
